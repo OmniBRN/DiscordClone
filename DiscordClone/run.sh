c6623f16f7a4755d0ddaf06586733e0ec88b841a
Tudor Boureanu <tudor04boureanu@gmail.com>
2024-11-06 23:42:17 +0200

    please

---
+++ DiscordClone/run.sh
@@ -0,0 +1,7 @@
+dotnet add package Microsoft.EntityFrameworkCore
+dotnet add package Microsoft.EntityFrameworkCore.Design
+dotnet add package Microsoft.EntityFrameworkCore.SqlServer
+dotnet add package Microsoft.EntityFrameworkCore.Tools
+
+dotnet new tool-manifest
+dotnet tool install --local dotnet-ef
