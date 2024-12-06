898d027aa71384699aeb52ab280f20bf627f705c
Tudor Boureanu <tudor04boureanu@gmail.com>
2024-12-06 14:31:35 +0200

    Am creat controllere pentru show, edit si delete

--- DiscordClone/Controllers/GroupsController.cs
+++ DiscordClone/Controllers/GroupsController.cs
@@ -19,18 +19,32 @@ namespace DiscordClone.Controllers
         private readonly UserManager<ApplicationUser> _userManager;
         private readonly RoleManager<IdentityRole> _roleManager;
         private readonly IWebHostEnvironment _env;
+
         public GroupsController(
-         ApplicationDbContext context,
-         UserManager<ApplicationUser> userManager,
-         RoleManager<IdentityRole> roleManager,
-         IWebHostEnvironment env
-         )
+            ApplicationDbContext context,
+            UserManager<ApplicationUser> userManager,
+            RoleManager<IdentityRole> roleManager,
+            IWebHostEnvironment env
+        )
         {
             db = context;
             _userManager = userManager;
             _roleManager = roleManager;
             _env = env;
         }
+
+        public IActionResult Show(int id)
+        {
+            var group = db.Groups.Where(o => o.Id == id).FirstOrDefault();
+            if (group == null)
+            {
+                TempData["NO_GROUP_SHOW"] = "Nu exista grupul pe care incerci sa-l accesezi";
+                return RedirectToAction("Groups", "Index");
+            }
+
+            return View(group);
+        }
+
 
         // Se afiseaza lista tuturor grupurilor in care nu te afli + categoria lor
         // Acum se afiseaza toate grupurile, modific filtrarea mai incolo
@@ -39,7 +53,7 @@ namespace DiscordClone.Controllers
         {
             var groups = db.Groups;
             ViewBag.Groups = groups;
-
+            
             if( TempData.ContainsKey("message"))
             {
                 ViewBag.Message = TempData["message"].ToString();
@@ -109,6 +123,70 @@ namespace DiscordClone.Controllers
                 group.Categ = GetAllCategories();
                 return View(group);
             }
+        }
+
+        public IActionResult Edit(int id)
+        {
+            var group = db.Groups.Where(o => o.Id == id).FirstOrDefault();
+            if (group == null)
+            {
+                TempData["message"] = "Nu exista grupul pe care incerci sa-l editezi";
+                TempData["messageType"] = "alert-danger";
+                return RedirectToAction("Index", "Groups");
+                
+            }
+            group.Categ = GetAllCategories();
+            return View(group);
+            
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Group editedGroup)
+        {
+            var group = db.Groups.Where(o => o.Id == editedGroup.Id).FirstOrDefault();
+            
+            if (group!=null && ModelState.IsValid)
+            {
+                group.Name = editedGroup.Name;
+                group.Description = editedGroup.Description;
+                group.CategoryId = editedGroup.CategoryId;
+                //Aici putem sa alegem daca vrem sa stergem poza veche a grupului de pe server sau nu
+                group.ImageRPath = editedGroup.ImageRPath;
+                TempData["message"] = "Grupul a fost modificat cu succes";
+                TempData["messageType"] = "alert-success";
+                db.SaveChanges();
+                // trebuie sa fac sa redirectionez in grupul pe care l-am modificat?
+                return RedirectToAction("Index", "Groups");
+            }
+            else if (group == null)
+            {
+                TempData["message"] = "Nu exista grupul pe care incerci sa-l editezi";
+                TempData["messageType"] = "alert-danger";
+                return RedirectToAction("Index", "Groups");
+                
+            }
+            else
+            {
+                editedGroup.Categ = GetAllCategories();
+                return View(editedGroup);
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Delete(int id)
+        {   
+            
+            var group = db.Groups.Where(o => o.Id == id).FirstOrDefault();
+            if (group == null)
+            {
+                TempData["message"] = "Grupul pe care incerci sa-l stergi nu exista";
+                TempData["messageType"] = "alert-danger";
+            }
+            db.Groups.Remove(group);
+            db.SaveChanges();
+            TempData["message"] = "Grupul a fost sters cu succes";
+            TempData["messageType"] = "alert-succes";
+            return RedirectToAction("Index", "Groups");
         }
 
         [NonAction]
